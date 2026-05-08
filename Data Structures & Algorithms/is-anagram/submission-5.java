4bbb23fda87fe7e20a4a27009e812551828dd70b
Erfan Hosseini <93828870+3rfan@users.noreply.github.com>
2026-05-08 11:12:29 +0200

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.java
@@ -0,0 +1,67 @@
+class Solution {
+    //input: s, t
+    //types: String
+    
+    //Goal:
+    // - if s and t contain the exact same characters (order doesn't matter) -> return true
+    // - else -> return false
+
+    //Initial plan:
+    // - if length of s != t -> return false
+    // - loop over characters in s, put into hashmap
+    // - loop over characters in t, put into hashmap
+    // - compare both hashmaps and if they have a mismatch in characters/values -> return false
+    // - else -> return true
+
+    //Complexities:
+    // - space: O(1)
+    // - time: O(n + m) + hashing costs
+
+    //Optimize:
+    // - since we only use lowercase english alphabet letters we can use an array
+    // - create single int arr with 26 entries
+    // - iterate through both s and t
+    // - - for s, increment position i - 'a'
+    // - - for t, decrement position i - 'a'
+    // - then iterate through whole int arr, if any entry != 0 -> return false
+    // - else -> return true
+
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] chars = new int[26];
+        for (int i = 0; i < s.length(); i++){
+            chars[s.charAt(i) - 'a']++;
+            chars[t.charAt(i) - 'a']--;
+        }
+
+        for (int i : chars){
+            if (i != 0) return false;
+        }
+
+        return true;
+    }
+
+    // public boolean isAnagram(String s, String t) {
+    //     if (s.length() != t.length()) return false;
+    //     HashMap<Character, Integer> mapS = new HashMap<>();
+    //     HashMap<Character, Integer> mapT = new HashMap<>();
+    //     for (char c : s.toCharArray()){
+    //         if (mapS.containsKey(c)){
+    //             mapS.put(c, mapS.get(c) + 1);
+    //         } else {
+    //             mapS.put(c,1);
+    //         }
+    //     }
+    //     for (char c : t.toCharArray()){
+    //         if (mapT.containsKey(c)){
+    //             mapT.put(c, mapT.get(c) + 1);
+    //         } else {
+    //             mapT.put(c,1);
+    //         }
+    //     }
+
+    //     if (mapS.equals(mapT)) return true;
+    //     return false;
+    // }
+}
